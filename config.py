f1bb31a9bde517072e2673d4a91fa97a188545b2
code-wolf-byte <75970974+code-wolf-byte@users.noreply.github.com>
2022-07-16 00:55:49 +0530

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-################################## Configuration ##################################
-input_guild_id = ""  # The ID of the server that you're copying
-output_guild_id = ""  # The ID of the server that you're pasting onto
-token = ""  # <-- Account token. (user)
-clear_server = True # Whether to clear the server you're pasting to or not (To delete old channels, roles, etc)
-windows = True # Whether you're on windows or not (for titling)
-###################################################################################
